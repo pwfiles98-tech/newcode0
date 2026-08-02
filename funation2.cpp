4a3479e5fe8510ce40f21589be7be52e1788d6f2
pwfiles98-tech <pwfiles98@gmail.com>
2026-08-03 03:32:14 +0530

    Update funation2.cpp

--- funation2.cpp
+++ funation2.cpp
@@ -2,9 +2,24 @@
 using namespace std;
 
 
-int main (){
+// find factorial of a number
+ int factorial(int n)
+ {
+    int fact = 1;
+    for(int i=1; i<=n; i++){
+        // fact = fact * i;
+        fact *= i;
 
- cout<< "hello\n";
+    }
+    return fact;
+}
+//here help to cout code  
+int main () {
+    cout << factorial(4) << endl;
+    cout << factorial(3) << endl;
+
+
+
 
 
     return 0;
